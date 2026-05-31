0c39beac310db29089d84cbafa75e2b4d28ff83f
Vladimir Popov <rebimol@gmail.com>
2026-05-31 18:13:00 +0200

    Add copyright file headers

--- registration.php
+++ registration.php
@@ -1,20 +1,8 @@
 <?php
 /**
- * MageMe
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the MageMe.com license that is
- * available through the world-wide-web at this URL:
- * https://mageme.com/license
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade this extension to newer
- * version in the future.
- *
- * Copyright (c) MageMe (https://mageme.com)
- **/
+ * Copyright © MageMe. All rights reserved.
+ * See LICENSE for license terms, or https://mageme.com/license.
+ */
 
 use Magento\Framework\Component\ComponentRegistrar;
 
